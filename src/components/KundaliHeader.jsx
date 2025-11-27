6ce2cd74a3d361490743aea7252913c603cd6b9f
Nitishjhaa <nj9876543210p@gmail.com>
2025-11-27 22:53:31 +0530

    made app drawer

--- src/components/KundaliHeader.jsx
+++ src/components/KundaliHeader.jsx
@@ -27,7 +27,7 @@ export default function KundaliHeader({
         setIsSideOpen(true);
       }
 
-      // Swipe to CLOSE
+      // Swipe to CLOSE when drawer open
       if (isSideOpen && startX > 240 && startX - currentX > 50) {
         setIsSideOpen(false);
       }
@@ -44,12 +44,20 @@ export default function KundaliHeader({
 
   return (
     <>
+      {/* LEFT EDGE SWIPE OPENER */}
+      <div
+        className="fixed top-0 left-0 h-screen w-5 z-50"
+        onTouchStart={() => setIsSideOpen(true)}
+      ></div>
+
       {/* ---------- TOP HEADER ---------- */}
       <div className="rounded-3xl overflow-hidden mb-4">
         <div className="bg-linear-to-r from-[#FFE984] to-[#FFB111] p-5 flex gap-4 items-center">
           <img
             src="/images/kundaliHead.png"
-            className={`${isSideOpen ? "rotate-180" : ""} transition-all duration-300 w-12 brightness-0`}
+            className={`${
+              isSideOpen ? "rotate-180" : ""
+            } transition-all duration-300 w-12 brightness-0`}
             onClick={() => setIsSideOpen(!isSideOpen)}
           />
           <span className="bg-linear-to-l from-[#F26A20]/50 to-red-500 bg-clip-text text-transparent text-xl">
@@ -69,14 +77,16 @@ export default function KundaliHeader({
       {/* ---------- FRAMER MOTION SIDE DRAWER ---------- */}
       <motion.div
         initial={{ x: -260 }}
-        animate={{ x: isSideOpen ? 8 : -260 }}
+        animate={{ x: isSideOpen ? 0 : -260 }}   // FULL OPEN = 0
         transition={{ type: "spring", stiffness: 260, damping: 30 }}
-        className="fixed top-2 left-0 h-screen w-70 bg-black z-20 rounded-3xl p-3"
+        className="fixed top-0 left-0 h-screen bg-black z-20 rounded-r-3xl p-3"
         style={{ width: "260px" }}
       >
         <img
           src="/images/kundaliHead.png"
-          className={`${isSideOpen ? "rotate-180" : ""} transition-all duration-300 w-12 brightness-0 invert-100`}
+          className={`${
+            isSideOpen ? "rotate-180" : ""
+          } transition-all duration-300 w-12 brightness-0 invert-100`}
           onClick={() => setIsSideOpen(false)}
         />
 

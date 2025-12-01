a7f80b55295aa897dc6ac2d7cfabc629b3a8c88f
Nitishjhaa <nj9876543210p@gmail.com>
2025-12-01 23:22:24 +0530

    made d1 to d60 kundali

--- src/app/kundaliInfo/sodasVargaKundali/SodasInner.jsx
+++ src/app/kundaliInfo/sodasVargaKundali/SodasInner.jsx
@@ -5,15 +5,23 @@ import { loadKundaliByIndex } from "@/lib/db";
 import { useSearchParams, useRouter } from "next/navigation";
 import KundaliStructure from '@/components/KundaliStructure';
 import PlanetTable from "@/components/PlanetTable";
-import PlanetAspectTable from "@/components/PlanetAspectTable";
-import KundaliHeader from '@/components/KundaliHeader'
+import KundaliHeader from '@/components/KundaliHeader';
+import { computeD9Chart } from "@/lib/computeD9";
+import NavamshaPlanetTable from '@/components/NavamshaPlanetTable'
+
+
 
 export default function KundaliInfoInner() {
     const [kundali, setKundali] = useState(null);
     const [isSideOpen, setIsSideOpen] = useState(false)
     const params = useSearchParams();
     const router = useRouter();
-    const [kundaliNumber, setKundaliNumber] = useState(null)
+    const [d9, setD9] = useState(null);
+    const [selectedChart, setSelectedChart] = useState("लग्न / D1");
+
+    const charts = [
+        "लग्न / D1", "होरा / D2", "द्रेक्काण / D3", "चतुर्थांश / D4", "सप्तमांश / D7", "नवमांश / D9", "दशमांश / D10", "द्वादशांश / D12", "षोडशांश / D16", "विंशांश / D20", "चतुर्विंशांश(सिद्धांश) / D24", "सप्तविंशांश (भाम्स) / D27", "त्रिंशांश / D30", "खवेदांश / D40", "अक्षवेदांश / D45", "षष्ट्यांश / D60", " / Moon", " / Sun"
+    ];
 
     const indexParam = params.get("index");
 
@@ -28,30 +36,60 @@ export default function KundaliInfoInner() {
             if (!record) return;
 
             setKundali(record);
+
+            // Compute and store D9
+            const d9Chart = computeD9Chart(record.raw);
+            setD9(d9Chart);
         }
         load();
     }, [indexParam, router]);
-
-
-    // if (!kundali) return <div className="p-4 text-white">Loading...</div>;
-    // if (!kundali?.raw?.ascendant?.rashiIndex) return <div className="p-4 text-white">Loading...</div>;
 
     return (
         <div className="p-2 overflow-hidden text-black" >
             <div className="w-[98%] mx-auto">
 
-                <div className="">
-                    <KundaliHeader
-                        title="अन्य वर्ग-कुंडली"
-                        indexParam={indexParam}
-                        isSideOpen={isSideOpen}
-                        setIsSideOpen={setIsSideOpen}
-                    />
-                    <div className="flex flex-col justify-center items-center bg-linear-to-r from-[#FFE984] to-[#FFB111] rounded-3xl pb-10">
-                        <KundaliStructure kundali={kundali} title="लग्न कुंडली" />
-                        <PlanetTable kundali={kundali} />
+                <KundaliHeader
+                    title="अन्य वर्ग-कुंडली"
+                    indexParam={indexParam}
+                    isSideOpen={isSideOpen}
+                    setIsSideOpen={setIsSideOpen}
+                />
 
-                    </div>
+                {/* Dropdown Selector */}
+                <div className="flex justify-center my-4">
+                    <select
+                        value={selectedChart}
+                        onChange={(e) => setSelectedChart(e.target.value)}
+                        className="p-2 w-full h-15 rounded-3xl border border-gray-300 bg-linear-to-r from-[#FFE984] to-[#FFB111] text-black shadow-sm focus:outline-none focus:ring-2 focus:ring-yelloe-500 text-center"
+                    >
+                        {charts.map((chart) => (
+                            <option key={chart} value={chart}>
+                                {chart}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+
+                <div className="flex flex-col justify-center items-center bg-linear-to-r from-[#FFE984] to-[#FFB111] rounded-3xl pb-10">
+                    {selectedChart === "लग्न / D1" && (
+                        <>
+                            <KundaliStructure kundali={kundali} title="लग्न कुंडली" />
+                            <PlanetTable kundali={kundali} />
+                        </>
+                    )}
+
+                    {selectedChart === "नवमांश / D9" && (
+                        <>
+                            <KundaliStructure kundali={d9} title="नवमांश कुंडली" />
+                            <NavamshaPlanetTable d9={d9} />
+                        </>
+                    )}
+
+                    {!["लग्न / D1", "नवमांश / D9"].includes(selectedChart) && (
+                        <div className="p-10 text-center text-lg font-semibold">
+                            {selectedChart} Chart Coming Soon...
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
